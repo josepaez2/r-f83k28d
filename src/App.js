450d4c557133c2638995f2cf5ae0e44c7e700107
josepaez <josepaez_2@hotmail.com>
2019-05-27 15:59:21 -0500

    Adds solution

--- src/App.js
+++ src/App.js
@@ -1,33 +1,61 @@
+// import React from 'react';
+// import './App.css';
+
+// class App extends React.Component {
+//   state = {
+//     // valor: 1,
+//     // nombre: "",
+//     // lastName: "",
+//     value: 0
+//   }
+//   handler = () => {
+//     // const {name, value} = target
+//     // this.state.name = event.target.value
+//     // this.setState({[value]: value + 1})
+//     this.setState({ value: this.state.value + 1 })
+//   }
+//   // saveUser = (event) => {
+
+//   // }
+//   render() {
+//     // console.log("Cambié")
+//     return (
+//       <div>
+//         <h1>Contador</h1>
+//         <p name="value" value={this.state.value}>{this.state.value}</p>
+//         <button name="nada" type="text" onClick={this.handler}>Incrementa</button>
+//         {/* <input name="nombre" type="text" onChange={this.handler}></input> */}
+//         {/* <p name="name">{this.state.value}</p> */}
+//         {/* <p name="name">{console.log(this.state)}</p> */}
+//         {/* <input name="lastName" type="text" onChange={this.handler.lastName}></input> */}
+//       </div>
+
+//     )
+//   }
+
+// }
+
+// export default App;
+
+
 import React, { Component } from 'react';
 import './App.css';
 
-class App extends React.Component {
+class App extends Component {
   state = {
-    valor: 1,
-    nombre: "",
-    lastName: ""
+    value: 0
   }
-  handler = ({target}) => {
-    const {name, value} = target
-    // this.state.name = event.target.value
-    this.setState({[name]: value})
+  handler = () => {
+    this.setState({ value: this.state.value + 1 })
   }
-  // saveUser = (event) => {
-
-  // }
   render() {
-    console.log("Cambié")
     return (
       <div>
-        <h1>Repetidor</h1>
-        <input name="nombre" type="text" onChange={this.handler}></input>
-        <p>{this.state.nombre}</p>
-        {/* <input name="lastName" type="text" onChange={this.handler.lastName}></input> */}
+        <span className="value">{this.state.value}</span>
+        <button id="inc" onClick={this.handler}>Incrementa</button>
       </div>
-
-    )
+    );
   }
-
 }
 
 export default App;
